c0765eecdc0fe8062f82e35be025e667adc54956
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-07-14 11:43:54 -0400

    fix: wrong define

--- top_sim.h
+++ top_sim.h
@@ -1,11 +1,11 @@
-#ifndef ORCA_H  
-#define ORCA_H
+#ifndef TOP_SIM_H
+#define TOP_SIM_H
 
 #include <string>
 
-namespace top {
-    std::string top_sim(std::string gfin, std::string hfin); 
+namespace top
+{
+  std::string top_sim(std::string gfin, std::string hfin);
 }
 
 #endif
-
